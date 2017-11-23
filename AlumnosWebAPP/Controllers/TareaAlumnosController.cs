ef47025b7393ebd567213a270dcf571cd062957e
StenOner <stenoner90@gmail.com>
2017-11-22 21:27:34 -0500

    actualizado

---
+++ AlumnosWebAPP/Controllers/TareaAlumnosController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+//using System.Data.Entity;
+//using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+//using AlumnosWebAPP.Models;
+using System.Collections.Generic;
+
+namespace AlumnosWebAPP.Controllers
+{
+    public class TareaAlumnosController : ApiController
+    {
+
+    }
+}
