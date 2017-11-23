4a4f0a2580ce9ee9ff90cc4be75e7032cc799408
AEPIS01 <fannyluz@gmail.com>
2017-11-22 21:50:25 -0500

    atributos de la clase alumno

--- AlumnosWebAPP/Models/Alumno.cs
+++ AlumnosWebAPP/Models/Alumno.cs
@@ -8,5 +8,10 @@ namespace AlumnosWebAPP.Models
     public class Alumno
     {
         public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String FotoURL { get; set; }
+        public String Usuario { get; set; }
+        public String Password { get; set; }
+
     }
 }
