81b07834d5cbddb2411edcc24e7b52ecd6ce99d1
StenOner <stenoner90@gmail.com>
2017-11-22 21:37:40 -0500

    Tarea/Models

--- AlumnosWebAPP/Models/Tarea.cs
+++ AlumnosWebAPP/Models/Tarea.cs
@@ -7,5 +7,10 @@ namespace AlumnosWebAPP.Controllers
 {
     public class Tarea
     {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string ArchivoURL { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public DateTime FechaLimite { get; set; }
     }
 }
